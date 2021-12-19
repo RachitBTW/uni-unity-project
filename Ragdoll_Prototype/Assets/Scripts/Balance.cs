a621e8b9f3535c872e86d16c01ad0ab860e57fce
Rachit Agarwal <46292553+RachitBTW@users.noreply.github.com>
2021-12-19 15:56:07 +0530

    Push_02: Prototype_V0.1
    Imported old project- balance and walk.

---
+++ Ragdoll_Prototype/Assets/Scripts/Balance.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Balance : MonoBehaviour
+{
+    public float targetRotation;
+    Rigidbody2D rb;
+    public float force;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();   
+    }
+
+
+    void Update()
+    {
+        rb.MoveRotation(Mathf.LerpAngle(rb.rotation, targetRotation, force * Time.deltaTime));
+    }
+}
